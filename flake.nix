0a3d7f60d98735de67aa33cbd04d80f88de81d89
Michael Strobel <mstrobel97@gmail.com>
2025-03-11 08:04:09 +0100

    chore: update nix files

--- flake.nix
+++ flake.nix
@@ -6,39 +6,47 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }: let
-    plugin-overlay = final: prev: {
-      fake-nvim = (final.pkgs.vimUtils.buildVimPlugin {
-        name = "fake.nvim";
-        src = self;
-      }).overrideAttrs {
-        doCheck = false;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      plugin-overlay = final: prev: {
+        fake-nvim =
+          (final.pkgs.vimUtils.buildVimPlugin {
+            name = "fake.nvim";
+            src = self;
+          }).overrideAttrs
+            {
+              doCheck = false;
+            };
       };
-    };
 
-    supportedSystems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-  in
-    flake-utils.lib.eachSystem supportedSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          plugin-overlay
-        ];
-      };
-    in {
-      packages = rec {
-        default = fake-nvim;
-        inherit (pkgs) fake-nvim;
-      };
-    })
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            plugin-overlay
+          ];
+        };
+      in
+      {
+        packages = rec {
+          default = fake-nvim;
+          inherit (pkgs) fake-nvim;
+        };
+      }
+    )
     // {
       overlays.default = plugin-overlay;
     };
